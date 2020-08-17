d2e185e96351d035eef5deb43774207500ecacc4
kiforks <ks.kifor@gmail.com>
2020-08-17 16:29:56 +0300

    Fixed

--- uncompressed/js/scripts/scroll-behavior.js
+++ uncompressed/js/scripts/scroll-behavior.js
@@ -2,15 +2,19 @@ const anchors = document.querySelectorAll('.scroll');
 const contactsNav = document.querySelector('.contacts-nav');
 let isResizeble = true;
 
+
 setTimeout(() => {
-  if(isResizeble && contactsNav && window.pageYOffset > 100) {
-    window.addEventListener('scroll', () => contactsNav.classList.add('contacts-nav--fixed'));
+  try {
+    if(isResizeble && contactsNav && window.pageYOffset > 100) {
+      window.addEventListener('scroll', () => contactsNav.classList.add('contacts-nav--fixed'));
 
-    isResizeble = false;
-  } else if(window.pageYOffset < 100) {
-    contactsNav.classList.add('contacts-nav--fixed');
+      isResizeble = false;
+    } else if(window.pageYOffset < 100) {
+      contactsNav.classList.add('contacts-nav--fixed');
+    }
+  } catch {
+    let test = null;
   }
-
 }, 500)
 
 for (let anchor of anchors) {
@@ -31,7 +35,6 @@ const toggleClass = document.querySelector('.header');
 setTimeout(() => {
   window.addEventListener('scroll',  () => {
     if (window.pageYOffset > toggleClass.getBoundingClientRect().bottom - 75) {
-      // document.querySelector('.contacts-nav').classList.add('contacts-nav--fixed');
       toggleClass.classList.add('header--fixed');
     } else {
       toggleClass.classList.remove('header--fixed');
